9c349d0e382941a86823573788166516519e19cf
Helen <grits.lena@gmail.com>
2022-04-06 10:24:55 +0300

    check basic work

--- src/main/java/by/grits/Main.java
+++ src/main/java/by/grits/Main.java
@@ -1,8 +1,27 @@
 package by.grits;
 
-public class Main {
+import by.grits.entity.Text;
+import by.grits.parsers.IParser;
+import by.grits.parsers.TextParser;
+import by.grits.utils.FileReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-  public static void main(String[] args) {
-    // write your code here
+import java.io.IOException;
+
+public class Main {
+  private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+  public static void main(String[] args) throws IOException {
+    String inputText;
+    FileReader fileReader = new FileReader();
+    inputText = fileReader.readFromFile("TextToParse.txt");
+    LOGGER.info("Text from file: \n" + inputText);
+
+    IParser textParser = new TextParser();
+
+    Text textComposite = textParser.parseText(inputText);
+
+    LOGGER.info("Parsed text:\n" + textComposite.getContents());
   }
 }
