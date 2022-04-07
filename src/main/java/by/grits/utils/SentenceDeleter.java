a2dd94035e5bcb310d188f8ec979cff62cad16c2
Helen <grits.lena@gmail.com>
2022-04-07 13:24:01 +0300

    minor fixes

--- src/main/java/by/grits/utils/SentenceDeleter.java
+++ src/main/java/by/grits/utils/SentenceDeleter.java
@@ -1,5 +1,6 @@
 package by.grits.utils;
 
+import by.grits.entity.CompositeType;
 import by.grits.entity.Text;
 import by.grits.entity.TextComposite;
 import org.apache.logging.log4j.LogManager;
@@ -7,7 +8,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SentenceDeleter {
   private static final Logger LOGGER = LogManager.getLogger(SentenceDeleter.class);
@@ -18,13 +18,14 @@ public class SentenceDeleter {
     for (Text paragraph : paragraphs) {
       sentences.addAll(paragraph.getTextComponents());
     }
-    for(int i =0; i<sentences.size(); i++){
-      if(sentences.get(i).getTextComponents().size() < wordsAmount + 4){
+
+    for (int i = 0; i < sentences.size(); i++) {
+      if (sentences.get(i).getTextComponents().size() < wordsAmount + 4) {
         sentences.remove(i);
       }
     }
+
     TextComposite deletedText = new TextComposite(sentences);
     LOGGER.info("With deleted sentences:\n" + deletedText.getContents());
-    //LOGGER.info("Without some sentences:\n" + new TextComposite(reformattedText).getContents());
   }
 }
